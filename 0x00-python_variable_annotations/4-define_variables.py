018c4ed052d960e9bddce417621e76e3cd554d7a
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-04-19 20:48:34 +0100

    Variable annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" A script that contains python variable annotations
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
