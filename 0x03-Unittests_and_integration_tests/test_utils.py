6e82898283902402c41870bdbaf7e020aec515ab
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-05-13 11:06:54 +0100

    Test utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,13 +3,14 @@
 from parameterized import parameterized
 from utils import access_nested_map
 from utils import get_json
+from utils import memoize
 import unittest
-from unittest import mock
+from unittest.mock import patch
 import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Represents a test case for access_nested_map.
+    """Represents a test case for utils.access_nested_map.
     """
 
     @parameterized.expand([
@@ -33,17 +34,40 @@ class TestAccessNestedMap(unittest.TestCase):
 
 
 class TestGetJson(unittest.TestCase):
-    """Represents a test case for get_json.
+    """Represents a test case for utils.get_json.
     """
 
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    @mock.patch("requests.get")
+    @patch("requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
         """Tests get_json"""
         mock_get.return_value.json.return_value = test_payload
         response = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Represents a test case for utils.memoize.
+    """
+
+    def test_memoize(self):
+        """Tests memoize"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            instance = TestClass()
+            instance.a_property()
+            instance.a_property()
+
+            mock_method.assert_called_once()
